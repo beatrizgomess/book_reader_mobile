01b9cd9098c4caef4e51a0a17e509ccd1c804802
beatriz.gomes <beatrizgomesxx@gmail.com>
2022-04-14 00:04:42 -0300

    add inputs and button

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -4,10 +4,10 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(235, 249, 238, 114),
+      backgroundColor: Color.fromRGBO(236, 216, 118, 1),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.only(top: 20, left: 10, right: 10, bottom: 10),
+          padding: EdgeInsets.only(top: 20, left: 15, right: 15, bottom: 10),
           child: Column(
             children: [
               Row(
@@ -40,14 +40,93 @@ class Login extends StatelessWidget {
               //   ),
               // ),
               const SizedBox(
-                height: 10,
+                height: 15,
               ),
 
               Container(
                 height: 2,
                 width: 500,
                 color: Colors.white,
-              )
+              ),
+
+              const SizedBox(
+                height: 70,
+              ),
+
+              TextFormField(
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.all(5),
+                  fillColor: Colors.white,
+                  filled: true,
+                  label: Text(
+                    'Email',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Lucida Console',
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+
+              TextFormField(
+                decoration: const InputDecoration(
+                  contentPadding: EdgeInsets.all(5),
+                  fillColor: Colors.white,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  label: Text(
+                    'Senha',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Lucida Console',
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+
+              OutlinedButton(
+                style: ButtonStyle(
+                  fixedSize: MaterialStateProperty.all<Size>(
+                    const Size.fromWidth(80),
+                  ),
+                  shape: MaterialStateProperty.all<OutlinedBorder>(
+                    const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                  elevation: MaterialStateProperty.all<double>(2),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  overlayColor: MaterialStateProperty.all<Color>(
+                    const Color.fromARGB(255, 254, 254, 121),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Entrar',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'Lucida Console',
+                  ),
+                ),
+              ),
             ],
           ),
         ),
